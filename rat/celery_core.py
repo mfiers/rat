08578d64b5207df21a818f6aa81e47f1b646cbdb
Mark Fiers <mark.fiers.42@gmail.com>
2016-08-04 13:11:49 +0200

    create celery app function

---
+++ rat/celery_core.py
@@ -0,0 +1,23 @@
+
+
+
+import os
+from celery import Celery
+
+
+_APP = None
+
+def get_celery_app():
+    if _APP is None:
+        global _APP
+        broker = os.environ.get('CELERY_BROKER', 'redis://:muffins1@r10n1:6379/0')
+        backend = os.environ.get('CELERY_BACKEND', 'redis://:muffins1@r10n1:6379/0')
+        concurrency = int(os.environ.get('CELERYD_CONCURRENCY', 11))
+
+        broker_transport_options = {'fanout_patterns': True,
+                                    'fanout_prefix': True}
+
+        _APP = Celery('tasks', broker=broker, backend=backend)
+        _APP.conf.CELERYD_POOL_RESTARTS = True
+        _APP.conf.CELERYD_CONCURRENCY = concurrency
+    return _APP
